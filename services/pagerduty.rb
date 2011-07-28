4098316f9e66ef15bde55eb74646251698bd5da1
Eric Lindvall <eric@5stops.com>
2011-07-28 15:53:42 -0700

    Fix Pagerduty webhook.

--- services/pagerduty.rb
+++ services/pagerduty.rb
@@ -1,4 +1,7 @@
 # encoding: utf-8
+
+# Initial implementation by Mike Heffner:
+#  https://github.com/librato/papertrail_pagerduty_webhook
 class Service::Pagerduty < Service
   def receive_logs
     body = {
@@ -12,10 +15,6 @@ class Service::Pagerduty < Service
 
     body[:incident_key] = settings[:incident_key] if settings[:incident_key].present?
 
-    body[:details] = {
-
-    }
-
     if settings[:base_url].present?
       body[:details][:log_start_url] =
         "#{settings[:base_url]}?centered_on_id=#{payload[:min_id]}"
@@ -23,6 +22,6 @@ class Service::Pagerduty < Service
         "#{settings[:base_url]}?centered_on_id=#{payload[:max_id]}"
     end
 
-    http_post "https://events.pagerduty.com/generic/2010-04-15/create_event.json", body
+    http_post "https://events.pagerduty.com/generic/2010-04-15/create_event.json", body.to_json
   end
 end
