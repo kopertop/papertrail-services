0abd29697a5a9ffc6e0f3733c0d75e758cdcb31f
Eric Lindvall <eric@5stops.com>
2011-08-02 15:11:55 -0700

    Make sure we're passing the correct headers to silverline.

--- services/silverline.rb
+++ services/silverline.rb
@@ -3,7 +3,11 @@ class Service::Silverline < Service
   def receive_logs
     count = payload[:events].length
 
-    http_post 'https://metrics-api.librato.com/v1/metrics.json' do |req|
+    http.basic_auth settings[:user], settings[:token]
+
+    res = http_post 'https://metrics-api.librato.com/v1/metrics.json' do |req|
+      req.headers[:content_type] = 'application/json'
+
       req.body = {
         :gauges => {
           settings[:name] => {
