35c572ccf19bf2e28f1a2b36bf75ede0df218ff2
Troy Davis <troy@sevenscale.com>
2011-08-11 23:00:39 -0700

    trim more metric preprocessing

--- services/cloud_watch.rb
+++ services/cloud_watch.rb
@@ -2,7 +2,7 @@ class Service::CloudWatch < Service
   attr_writer :acw
   
   def receive_logs
-    data_points = PapertrailServices::Helpers::EventMetrics.prepare(payload[:events], settings[:name], settings[:regex])
+    data_points = prepare(payload[:events], settings[:name], settings[:regex])
         
     return if data_points.empty?
 
@@ -12,4 +12,46 @@ class Service::CloudWatch < Service
   def acw
     @acw ||= RightAws::AcwInterface.new(settings[:access_key_id], settings[:secret_key])
   end
+  
+  def prepare(events, name, regex)
+    unless regex
+      # count all events as a single data point
+      return [{ 
+        :metric_name => name,
+        :value => events.length,
+        :timestamp => Time.now.utc.strftime('%FT%XZ')
+      }]
+    end
+
+    data_points_for_metric(name, events, Regexp.new(metric[:regex]))
+  end
+  
+  def data_points_for_metric(name, events, regex)
+    data_points = []
+          
+    events.each do |event|
+      value = nil
+      if regex
+        match_data = event[:message].match(regex)
+        # regex provided and does not match event
+        next unless match_data
+    
+        # use extracted value
+        value = match_data[1].to_i if match_data[1]
+      end
+
+      # no regex used (or no backref used), so this is basically an 
+      # unaggregated counter
+      value ||= 1
+  
+      data_points << { 
+        :metric_name => name,
+        :value => value,
+        :timestamp => event[:received_at]
+      }
+    end
+    
+    data_points
+  end
+  
 end
