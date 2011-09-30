c55cc1cd09168ae10222b489bf3cb56dd3ad9d4f
Troy Davis <troy@sevenscale.com>
2011-09-30 11:32:26 -0700

    a bit of cleanup for clarity

--- services/cloud_watch.rb
+++ services/cloud_watch.rb
@@ -2,11 +2,18 @@ class Service::CloudWatch < Service
   attr_writer :acw
   
   def receive_logs
-    data_points = prepare(payload[:events], settings[:name], settings[:regex])
+    data_points = prepare(payload[:events], settings[:metric_name], settings[:regex])
+    puts data_points.inspect
         
     return if data_points.empty?
 
-    acw.put_metric_data(:namespace => settings[:namespace] || 'Papertrail', :data => data_points)
+    if settings[:metric_namespace].present?
+      metric_namespace = settings[:metric_namespace]
+    else
+      metric_namespace = 'Papertrail'
+    end
+    
+    acw.put_metric_data(:namespace => metric_namespace, :data => data_points)
   end
 
   def acw
@@ -14,7 +21,7 @@ class Service::CloudWatch < Service
   end
   
   def prepare(events, name, regex)
-    unless regex
+    unless regex.present?
       # count all events as a single data point
       return [{ 
         :metric_name => name,
@@ -23,26 +30,21 @@ class Service::CloudWatch < Service
       }]
     end
 
-    data_points_for_metric(name, events, Regexp.new(regex))
+    extract_data_points(name, events, Regexp.new(regex))
   end
   
-  def data_points_for_metric(name, events, regex)
+  def extract_data_points(name, events, regex)
     data_points = []
           
     events.each do |event|
       value = nil
-      if regex
-        match_data = event[:message].match(regex)
-        # regex provided and does not match event
-        next unless match_data
-    
-        # use extracted value
-        value = match_data[1].to_i if match_data[1]
-      else
-        # no regex used (or no backref used), so this is basically an 
-        # unaggregated counter
-        value = 1
-      end
+
+      match_data = event[:message].match(regex)
+      # does not match event
+      next unless match_data
+  
+      # use extracted value or 1 (no regex backreference provided)
+      value = match_data[1] ? match_data[1].to_i : 1
   
       data_points << { 
         :metric_name => name,
@@ -53,5 +55,4 @@ class Service::CloudWatch < Service
     
     data_points
   end
-  
 end
